d1ce7366e812e2dcd5449ead645c16a57fe828fe
Amit Raj <rajamit31032004@gmail.com>
2025-07-03 19:21:55 +0530

    Add files via upload

--- app/login/page.js
+++ app/login/page.js
@@ -1,10 +1,15 @@
 "use client";
 
 import React, { useState } from "react";
+import { useRouter } from "next/navigation";
+import Toast from "@/app/component/Toast/page";
+import { toast, Bounce } from "react-toastify";
+import Link from "next/link";
 
 export default function LoginPage() {
   const [rollNo, setRollNo] = useState("");
   const [password, setPassword] = useState("");
+  const router = useRouter();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -19,45 +24,88 @@ export default function LoginPage() {
 
       const result = await res.json();
       if (result.success) {
-        alert("✅ Login successful!");
-        // Optionally, redirect or store user token
+        toast(result.message, {
+          position: "top-center",
+          autoClose: 2500,
+          hideProgressBar: false,
+          closeOnClick: false,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+          transition: Bounce,
+        });
+        setTimeout(()=>{
+           router.push('/user/dashboard');
+        },2500);
+       
       } else {
-        alert("❌ Login failed: " + result.error);
+        toast(result.message, {
+          position: "top-center",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: false,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+          transition: Bounce,
+        });
+        setPassword('');
+        setRollNo('');
       }
     } catch (error) {
-      alert("❌ Login error: " + error.message);
+      toast(result.message, {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: false,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        transition: Bounce,
+      });
     }
   };
 
   return (
-    <main className="min-h-screen flex items-center justify-center">
-      <div className="bg-white p-8 rounded-xl shadow-md w-full max-w-md">
-        <h2 className="text-2xl font-bold mb-6 text-center text-blue-700">Log In</h2>
-        <form onSubmit={handleSubmit} className="space-y-4">
-          <input
-            type="text"
-            placeholder="Roll Number"
-            value={rollNo}
-            onChange={(e) => setRollNo(e.target.value)}
-            className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
-            required
-          />
-          <input
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
-            required
-          />
-          <button
-            type="submit"
-            className="w-full bg-blue-600 text-white py-2 rounded-md hover:bg-blue-700 transition"
-          >
-            Log In
-          </button>
-        </form>
-      </div>
-    </main>
+    <>
+      <Toast />
+      <main className="min-h-screen flex items-center justify-center">
+        <div className="bg-white p-8 rounded-xl shadow-md w-full max-w-md">
+          <h2 className="text-2xl font-bold mb-6 text-center text-blue-700">Log In</h2>
+          <form onSubmit={handleSubmit} className="space-y-4">
+            <input
+              type="text"
+              placeholder="Roll Number"
+              value={rollNo}
+              onChange={(e) => setRollNo(e.target.value)}
+              className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
+              required
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
+              required
+            />
+            <button
+              type="submit"
+              className="w-full bg-blue-600 text-white py-2 rounded-md hover:bg-blue-700 transition"
+            >
+              Log In
+            </button>
+          </form>
+          <div className="flex justify-center gap-1 mt-2">
+            <div>Don't have account?</div>
+            <Link href={'/signup'} className="text-indigo-500">Sign up</Link>
+            
+          </div>
+        </div>
+      </main>
+    </>
   );
 }
